643f1a941cf427d1f7cd4f02695975399cbd6052
Dutta <biswarup.dutta@capgemini.com>
2025-08-17 19:51:12 +0530

    4 th push

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -6,7 +6,7 @@ export default function TabButton({ children, onSelect, isSeleted }) {
 
   return (
     <li>
-      <button className={isSeleted? 'active' : undefined} onClick={handleClick}>{children}</button>
+      <button className={isSeleted? 'active' : undefined} onClick={onSelect}>{children}</button>
     </li>
   );
 }
